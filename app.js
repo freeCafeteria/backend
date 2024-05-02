a51c1a697a3ea26a0136b9d4c0a3048547c10934
seehun <hwangbird99@gmail.com>
2024-05-02 14:24:27 +0900

    feat: redis에  급식소 정보 저장 후 반환

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ redisClient.on("error", (err) => {
 redisClient.connect().then(); // redis v4 연결 (비동기)
 const redisCli = redisClient.v4;
 
-// 공공데이터 불러오기
-const get_data = async () => {
+//redis에서 급식소 정보 들고오기
+redisClient.get("cafeterias", (err, value) => {
+  const value2 = JSON.parse(value);
+  console.log(value2);
+});
+
+// 공공데이터 불러와서 redis에 저장
+const set_data_in_redis = async () => {
   const baseURL = process.env.BASEURL;
   const encoding_key = process.env.API_KEY;
 
@@ -32,8 +38,15 @@ const get_data = async () => {
     const res = await axios.get(
       `${baseURL}?serviceKey=${encoding_key}&pageNo=1&numOfRows=100&type=json`
     );
-    // console.log('data_array: ', res.data.response.body.items);
-    console.log("data: ", res.data.response);
+    // console.log("data_array: ", res.data.response.body.items);
+    // console.log("data: ", res.data.response);
+    if (res.data.response) {
+      //redis에 공공데이터 저장
+      redisClient.set(
+        "cafeterias",
+        JSON.stringify(res.data.response.body.items)
+      );
+    }
   } catch (error) {
     console.log(error);
   }
@@ -45,7 +58,7 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   //서버 실행 시 공공 데이터 가져오기
-  get_data();
+  set_data_in_redis();
 
   //스케줄러 등록
   schedule.scheduleJob("0 0 0 * * *", () => {
@@ -53,5 +66,6 @@ app.listen(3000, () => {
       timeZone: "Asia/Seoul",
     });
     console.log(koreaTime);
+    set_data_in_redis(); // 자정마다 데이터 저장
   });
 });
